b532ae2033631717fcf4cfc1878daeeb51dc5b7c
syjin0630 <syjjj0630@gmail.com>
2021-12-30 23:45:10 +0900

    title complete✔️

---
+++ js/title.js
@@ -0,0 +1,83 @@
+const title = document.body.querySelector("h1");
+const dateBox = title.querySelector("date");
+const gradeForm = title.querySelector(".grade");
+const gradeInput = gradeForm.querySelector("input");
+const classForm = title.querySelector(".class");
+const classInput = classForm.querySelector("input");
+const gradeSpan = title.querySelector(".grade_span");
+const classSpan = title.querySelector(".class_span");
+
+const HIDDEN_CLASSNAME = "hidden";
+const GRADE_KEY = "grade";
+const CLASS_KEY = "class";
+
+function getDate() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const dateNumber = String(date.getDate()).padStart(2, "0");
+  let day = date.getDay();
+
+  if (day === 1) {
+    day = "MON";
+  } else if (day === 2) {
+    day = "TUE";
+  } else if (day === 3) {
+    day = "WED";
+  } else if (day === 4) {
+    day = "THU";
+  } else if (day === 5) {
+    day = "FRI";
+  } else if (day === 6) {
+    day = "SAT";
+  } else if (day === 0) {
+    day = "SUN";
+  }
+
+  dateBox.innerHTML = `${year}.${month}.${dateNumber}.${day}❤️ `;
+}
+
+function gradePush() {
+  const grade = gradeInput.value;
+  gradeInput.value = "";
+  localStorage.setItem(GRADE_KEY, grade);
+
+  paintingGrade(grade);
+}
+function paintingGrade(grade) {
+  gradeForm.classList.add(HIDDEN_CLASSNAME);
+  gradeInput.classList.add(HIDDEN_CLASSNAME);
+  gradeSpan.innerText = grade;
+  gradeSpan.classList.remove(HIDDEN_CLASSNAME);
+}
+function classPush() {
+  const class_n = classInput.value;
+  classInput.value = "";
+
+  localStorage.setItem(CLASS_KEY, class_n);
+
+  paintingClass(class_n);
+}
+function paintingClass(class_n) {
+  classForm.classList.add(HIDDEN_CLASSNAME);
+  classInput.classList.add(HIDDEN_CLASSNAME);
+  classSpan.innerText = `­ ${class_n}`;
+  classSpan.classList.remove(HIDDEN_CLASSNAME);
+}
+getDate();
+setInterval(getDate, 86400000);
+
+const savedGradeNumner = localStorage.getItem(GRADE_KEY);
+const savedClassNumner = localStorage.getItem(CLASS_KEY);
+
+if (savedGradeNumner !== null) {
+  paintingGrade(savedGradeNumner);
+} else {
+  gradeForm.addEventListener("submit", gradePush);
+}
+
+if (savedClassNumner !== null) {
+  paintingClass(savedClassNumner);
+} else {
+  classForm.addEventListener("submit", classPush);
+}
